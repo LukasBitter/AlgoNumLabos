721b2cf71d75a88864d2ea60c2b04a2d0fa374c3
LukasBitter <lukas.bitter@he-arc.ch>
2015-03-08 14:38:41 +0100

    test commit

--- Labo_01/floatencode.cpp
+++ Labo_01/floatencode.cpp
@@ -86,6 +86,7 @@ void FloatEncode::calcE()
     bitset_e = bitset<BITS_E>(exp+CONST_D);
     bitset_m = bitset<BITS_M>(M*pow(2, BITS_M));
     bitset_m<<=1;
+    // test commit
 }
 
 void FloatEncode::calcS()
