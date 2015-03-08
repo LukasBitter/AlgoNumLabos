3d9dfcd079845efed681691a83b55cfcf02b7dab
Margaux Divernois <margaux.divernois@gmail.com>
2015-03-08 20:46:52 +0100

    Fusion

--- Labo_01/floatencode.cpp
+++ Labo_01/floatencode.cpp
@@ -175,7 +175,8 @@ void FloatEncode::calcE()
         bitset_e = bitset<BITS_E>(0);
     else
         bitset_e = bitset<BITS_E>(exp+CONST_D);
-    bitset_m = bitset<BITS_M>(M*pow(2, BITS_M));
+
+    bitset_m = bitset<BITS_M>(M*pow(2, BITS_M+1));
     bitset_m<<=1;
 }
 
@@ -189,11 +190,15 @@ void FloatEncode::calcS()
 
 bool FloatEncode::checkSpecial(double value)
 {
-    double minvalue = 0/pow(2, BITS_M) * pow(2, 0-CONST_D);
+
+    double minprecision = pow(2, -1*BITS_M) * pow(2, 0-CONST_D);
     //bigger value for "E" = 30 (5bits) : pow(2, BITS_E)-2
     double maxvalue =  (pow(2, BITS_M)-1) / pow(2, BITS_M) * pow(2, (pow(2, BITS_E)-2) - CONST_D);
 
-    if (value == 0)
+
+    cout << "-----"<<maxvalue;
+
+    if (value == 0 || (value < minprecision && value > (-1*minprecision)) )
     {
         bitset_s[0] = 0;
         bitset_e = bitset<BITS_E>(0);
@@ -214,7 +219,8 @@ bool FloatEncode::checkSpecial(double value)
 
         return true;
     }
-    else if(value<minvalue)
+
+    else if(value < (-1*maxvalue))
     {
         //- infinite
         bitset_s[0] = 1;
@@ -282,6 +288,7 @@ FloatEncode FloatEncode::calculate(FloatEncode value1, FloatEncode value2)
     cout<<"  Le nombre 1 : "<< value1.get_s() << "|" << value1.get_e() << "|" << bitset_mcache1 <<endl;
     cout<<"  Le nombre 2 : "<< value2.get_s() << "|" << value2.get_e() << "|" << bitset_mcache2 <<endl;
 
+
 //    Right now, the subtraction is not working. But most is already implemented.
 //    if(value1.get_s() == 1 || value2.get_s() == 1)
 //    {
@@ -291,6 +298,7 @@ FloatEncode FloatEncode::calculate(FloatEncode value1, FloatEncode value2)
 //    {
         //Call the addition function.
         result.add(bitset_mcache1, bitset_mcache2);
+
 
         //Set the "s" bit
 //        if(value1.get_s()==1 && value2.get_s()==1)
@@ -394,6 +402,7 @@ void FloatEncode::inc(bitset<BITS_M> m)
 //Subtraction is not completely implemented.
 void FloatEncode::sub(bitset<BITS_M+1> bitset_mcache1, bitset<BITS_M+1> bitset_mcache2)
 {
+
     for(int i = 0; i<BITS_M; i++)
     {
        if(bitset_mcache1[i] == 0 && bitset_mcache2[i] == 0)
--- Labo_01/readme.txt
+++ Labo_01/readme.txt
@@ -11,3 +11,42 @@ Participants :
  Divernois Margaux  <margaux.divernois@he-arc.ch>
  Bitter Lukas       <lukas.bitter@he-arc.ch>
  Visinand Steve     <steve.visinand@he-arc.ch>
+
+
+*************************************************
+
+*** Limites du programme:
+
+les valeures maximum et minimum représentable par 
+l'encodage float sont 32752 et -32752. 
+Ensuite le programme considérera les valeures
+comme infini et moins infini.
+
+on obtient cette limite ainsi :
+
+  (2^n-1)/2^n * 2^e - d
+
+  ou e = (2^nbits_e) - 2
+  et n le nombre de bits de m
+
+
+*** Précision du programme:
+
+en dessous de 1.49012 *10^-8 ( et en dessus de
+-1.49012 *10^-8) le programme considérera la 
+valeure comme égale à 0.
+
+on obtient cette précision ainsi :
+
+  2^-n * 2^e-d
+
+  ou e = 0 
+
+
+*** choix du décalage d
+
+d est fixé avec la formule 2^E-1 ou E est égal
+au nombre de bit de "e"
+
+d est donc = 15
+
