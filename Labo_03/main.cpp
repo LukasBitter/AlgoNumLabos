d95538af7999ddffd4c8d10ff08e28c251e079f9
LukasBitter <lukas.bitter@he-arc.ch>
2015-03-24 11:13:17 +0100

    ajout calsse affichage + fichier readme

--- Labo_03/main.cpp
+++ Labo_03/main.cpp
@@ -13,8 +13,38 @@ using namespace std;
 
 int main()
 {
-    SquareMatrix sm(5);
-    sm.showMatrix();
-    
+    int rep = Affichage::menuBegin();
+
+    do{
+        switch(rep)
+        {
+
+        case 1:
+            rep = Affichage::menuBegin();
+            SquareMatrix sm(5);
+            sm.showMatrix();
+
+            break;
+
+        case 2:
+
+            rep = Affichage::menuBegin();
+            break;
+
+        case 3:
+            Affichage::readme();
+            rep = Affichage::menuBegin();
+            break;
+        case 4:
+            break;
+        default:
+            rep = Affichage::menuNotValid();
+            break;
+        }
+
+    }
+    while(rep != 4);
+
+
     return 0;
 }
---
+++ Labo_03/readme.txt
@@ -0,0 +1,22 @@
+*************************************************
+*** Laboratoire 2, cours algorithme numerique ***
+***                22.03.2015                 ***
+*************************************************
+
+ But : 
+  Trouver toutes les solutions de -100 a 100 avec
+  la methode du point fixe pour deux fontions 
+  donnees : sin( x ) – x/13 et x / (1-x^2)
+
+ Participants :
+  Divernois Margaux  
+      <margaux.divernois@he-arc.ch>
+  Bitter Lukas
+      <lukas.bitter@he-arc.ch>
+  Da Mota Marques Fabio
+      <FabioManuel.DaMotaMarques@he-arc.ch> 
+  Visinand Steve
+      <steve.visinand@he-arc.ch>
+
+
+*************************************************
