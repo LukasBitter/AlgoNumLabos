16f23ee5e9ed9c77bcf0220f9625ac21d3b4c691
Steve Visinand <visinandst@gmail.com>
2015-03-31 10:09:46 +0200

    repush

--- Labo_03/squarematrix.cpp
+++ Labo_03/squarematrix.cpp
@@ -77,3 +77,35 @@ void SquareMatrix::showMatrix()
         std::cout << "|\n";
     }
 }
+
+
+void SquareMatrix::diagonaliser()
+{
+    for(int i=1; i<cote; i++)
+    {
+        double m1 = matrix[i-1][i-1];
+        for(int j=i; j<cote; j++)
+        {
+            double m2 = matrix[j][i-1];
+            
+            multLine(j, m1); //multiplier toute la ligne
+            sustractLines(j, i-1, m2); //line(j) = line(j) - m*line(i-1)
+        }
+    }
+}
+void SquareMatrix::multLine(int line, double n)
+{
+    for (int i=0; i<cote; i++) {
+        matrix[line][i] = matrix[line][i] * n;
+    }
+}
+
+//line1 = line1 - m*line2
+void SquareMatrix::sustractLines(int line1, int line2, double m)
+{
+    for(int i=0; i<cote; i++)
+    {
+        matrix[line1][i] = matrix[line1][i]-m*matrix[line2][i];
+    }
+}
+
