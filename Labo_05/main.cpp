4f0d861ad5b2d8c85f149691df8ee8cda5a62fa0
Margaux Divernois <margaux.divernois@gmail.com>
2015-05-17 13:10:39 +0200

    Structure du Labo 5

---
+++ Labo_05/main.cpp
@@ -0,0 +1,53 @@
+/**
+    Main Class - Implementation File
+    Purpose: this is the Main of the program
+
+    @author Equipe 6 (Bitter Lukas, Da Mota Marques Fabio Manuel, Divernois Margaux, Visinand Steve)
+
+    last update : 17.05.2015
+*/
+
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+#include "affichage.h"
+
+using namespace std;
+
+/**  Main program */
+int main()
+{
+    static int rep = Affichage::menuBegin();
+
+    do{
+        switch(rep)
+        {
+        case 1:
+            rep = Affichage::menuBegin();
+            break;
+
+        case 2:
+            rep = Affichage::menuBegin();
+            break;
+
+        case 3:
+            // Print readme
+            Affichage::readme();
+            rep = Affichage::menuBegin();
+            break;
+
+            // Exit
+        case 4:
+            break;
+
+            // Invalid entry
+        default:
+            rep = Affichage::menuNotValid();
+            break;
+        }
+
+    }
+    while(rep != 4);
+
+    return 0;
+}
---
+++ Labo_05/readme.txt
@@ -0,0 +1,23 @@
+***************************************************
+*** Laboratoire 5, Cours Algorithmes Numeriques ***
+***                17.05.2015                   ***
+***************************************************
+
+ But :
+  Developper un programme permettant d’approximer PI avec au moins 17
+  decimales juste en utilisant la formule :
+
+        PI / 4 = Integrale de 0 a 1 de 1/1+x^2
+
+ Participants :
+  Divernois Margaux
+      <margaux.divernois@he-arc.ch>
+  Bitter Lukas
+      <lukas.bitter@he-arc.ch>
+  Da Mota Marques Fabio
+      <FabioManuel.DaMotaMarques@he-arc.ch>
+  Visinand Steve
+      <steve.visinand@he-arc.ch>
+
+
+*************************************************
